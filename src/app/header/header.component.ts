b143f0efd9bb32ab963b10c741382db06c986fc0
Amit Gupta <amit.gupta@daffodilsw.com>
2019-04-15 11:34:27 +0530

    nc

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Response } from "@angular/http";
+import { Recipe } from "../recipes/recipe.model";
 
 @Component({
     selector: 'app-header',
@@ -6,5 +9,18 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
+    constructor(private dataStorage: DataStorageService){}
 
+    onSaveData(){
+        this.dataStorage.storeRecipes()
+            .subscribe(
+                (response:Response)=>{
+                    console.log(response);
+                }
+            )
+    }
+
+    onFetchData(){
+        this.dataStorage.getRecipes();
+    }
 }
