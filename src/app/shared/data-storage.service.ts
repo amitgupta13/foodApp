7a027782bf3fb6c8d2ed23ea73385c6ff7554758
Amit Gupta <amit.gupta@daffodilsw.com>
2019-04-15 14:22:05 +0530

    nc

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,21 +3,27 @@ import { Http, Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import 'rxjs/Rx'
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService{
-    constructor(private http:Http, private recipeService: RecipeService){}
+    constructor(private http:Http, private recipeService: RecipeService,
+        private authService: AuthService){}
 
     storeRecipes(){
+        let token = this.authService.getToken();
         return this.http.put(
-            'https://http-8a947.firebaseio.com/recipes.json',
+            `https://http-8a947.firebaseio.com/recipes.json?auth=${token}`,
             this.recipeService.getRecipes()
         );   
     }
 
     getRecipes(){
+        let token = this.authService.getToken();
+        this.authService.getToken();
+
         this.http.get(
-            'https://http-8a947.firebaseio.com/recipes.json'
+            `https://http-8a947.firebaseio.com/recipes.json?auth=${token}`
         )
         .map((response: Response)=>{
             const recipes: Recipe[] = response.json();
